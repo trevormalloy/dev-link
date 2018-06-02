76bcccfbf73236ee8c53cc86cb80eadd283c044f
Trevor Malloy <trevoramalloy@gmail.com>
2018-06-02 10:30:42 -0700

    setting up passport and jwt user authentication

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 const keys = require('../../config/keys')
 
@@ -91,6 +92,18 @@ router.post('/login', (req, res) => {
             return res.status(400).json({password: 'Password incorrect'});
           }
         })
+    });
+});
+
+// @route   GET api/users/current
+// @desc    Return current user
+// @access  Private
+router.get('/current', passport.authenticate('jwt', { session: false}), 
+  (req, res) => {
+    res.json({
+      id: req.user.id,
+      name: req.user.name,
+      email: req.user.email
     });
 });
 
