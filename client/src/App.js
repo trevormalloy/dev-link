29f771eb0c8c627c64f210776a264fbe4d1ba938
Trevor Malloy <trevoramalloy@gmail.com>
2018-06-08 06:41:16 -0700

    checkpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Footer from './components/layout/Footer';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Footer from "./components/layout/Footer";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -17,8 +17,8 @@ class App extends Component {
           <Navbar />
           <Route exact path="/" component={Landing} />
           <div className="container">
-            <Route exact path="/register" component={ Register } />
-            <Route exact path="/login" component={ Login } />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
           </div>
           <Footer />
         </div>
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 class Register extends Component {
   constructor() {
     super();
     this.state = {
-      name: '',
-      email: '',
-      password: '',
-      password2: '',
+      name: "",
+      email: "",
+      password: "",
+      password2: "",
       errors: {}
     };
 
@@ -16,7 +16,7 @@ class Register extends Component {
   }
 
   onChange(e) {
-    this.setState({[e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   onSubmit(e) {
@@ -27,7 +27,7 @@ class Register extends Component {
       email: this.state.email,
       password: this.state.password,
       password2: this.state.password2
-    }
+    };
 
     console.log(newUser);
   }
@@ -39,48 +39,53 @@ class Register extends Component {
           <div className="row">
             <div className="col-md-8 m-auto">
               <h1 className="display-4 text-center">Sign Up</h1>
-              <p className="lead text-center">Create your DevConnector account</p>
+              <p className="lead text-center">
+                Create your DevConnector account
+              </p>
               <form onSubmit={this.onSubmit}>
                 <div className="form-group">
-                  <input 
-                    type="text" 
-                    className="form-control form-control-lg" 
-                    placeholder="Name" 
-                    name="name" 
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Name"
+                    name="name"
                     value={this.state.name}
                     onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
-                  <input 
-                    type="email" 
-                    className="form-control form-control-lg" 
-                    placeholder="Email Address" 
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
                     name="email"
                     value={this.state.email}
                     onChange={this.onChange}
                   />
-                  <small className="form-text text-muted">This site uses Gravatar so if you want a profile image, use a Gravatar email</small>
+                  <small className="form-text text-muted">
+                    This site uses Gravatar so if you want a profile image, use
+                    a Gravatar email
+                  </small>
                 </div>
                 <div className="form-group">
-                  <input 
-                    type="password" 
-                    className="form-control form-control-lg" 
-                    placeholder="Password" 
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
                     name="password"
                     value={this.state.password}
                     onChange={this.onChange}
                   />
                 </div>
                 <div className="form-group">
-                  <input 
-                    type="password" 
-                    className="form-control form-control-lg" 
-                    placeholder="Confirm Password" 
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Confirm Password"
                     name="password2"
                     value={this.state.password2}
                     onChange={this.onChange}
-                />
+                  />
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
               </form>
@@ -88,7 +93,7 @@ class Register extends Component {
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
