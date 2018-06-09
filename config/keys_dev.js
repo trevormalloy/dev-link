22b06667b29ce3aa2a579b9e02152276a90ef22a
Trevor Malloy <trevoramalloy@gmail.com>
2018-06-09 09:29:50 -0700

    update

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://admin:ragequit1@ds149279.mlab.com:49279/dev-link',
+  mongoURI: 'mongodb://trevor:password1@ds149279.mlab.com:49279/dev-link',
   secretOrKey: 'secret'
 };
