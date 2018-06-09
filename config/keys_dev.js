876524048cde4526d66cc5d91014797b78473d30
Trevor Malloy <trevoramalloy@gmail.com>
2018-06-08 21:59:15 -0700

    update

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,4 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://admin:password@ds149279.mlab.com:49279/dev-link',
+  mongoURI: 'mongodb://admin:ragequit1@ds149279.mlab.com:49279/dev-link',
   secretOrKey: 'secret'
 };
