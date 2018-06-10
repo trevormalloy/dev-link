fb6770407c9a8be73a0c2be205c53cf80809cced
Trevor Malloy <trevoramalloy@gmail.com>
2018-06-10 14:40:06 -0700

    revert

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -34,13 +34,13 @@ class PostItem extends Component {
       <div className="card card-body mb-3">
         <div className="row">
           <div className="col-md-2">
-            <Link to={`/profile/${user}`}>
+            <a href="profile.html">
               <img
                 className="rounded-circle d-none d-md-block"
                 src={post.avatar}
                 alt=""
               />
-            </Link>
+            </a>
             <br />
             <p className="text-center">{post.name}</p>
           </div>
